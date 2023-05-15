85529e914ac399afd335c473049a8c7d6fecc63b
Deolu_Josef <kiishijoseph@gmail.com>
2023-05-15 15:07:30 +0100

    added popular jobs, nearby jobs UI, implemented their logics, added a custom fetch hook, fetched the search api from jserach on rapidapi

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native';
 
-import { View, Text } from 'react-native'
+import styles from './nearbyjobs.style';
 
-import styles from './nearbyjobs.style'
+import { COLORS } from '../../../constants';
+
+import NearByJobCard from '../../common/cards/nearby/NearbyJobCard';
+
+import useFetch from '../../../hook/useFetch';
 
 const Nearbyjobs = () => {
-  return (
-    <View>
-      <Text>Nearbyjobs</Text>
-    </View>
-  )
-}
+  const { data, isLoading, error } = useFetch('search', {
+    query: 'React developer',
+    num_pages: 1,
+  });
 
-export default Nearbyjobs
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Nearby Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>See all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          data?.map((job) => (
+            <NearByJobCard
+              job={job}
+              key={`nearby-job-${job?.job_id}`}
+              handleNavigate={() =>
+                useRouter.push(`/job-details/${job.job_id}`)
+              }
+            />
+          ))
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Nearbyjobs;
